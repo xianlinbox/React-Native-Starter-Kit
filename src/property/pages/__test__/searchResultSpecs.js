b9a3b07c34cd32f83e9eb123649bfc3207b6fa90
Liu Xianning <Liu.Xianing@wnco.com>
2016-09-15 20:50:18 +0800

    add babel, enzyme for react native unit test

---
+++ src/property/pages/__test__/searchResultSpecs.js
@@ -0,0 +1,8 @@
+import React, {View, Text, StyleSheet} from 'react-native';
+import {expect} from 'chai';
+
+describe('searchResultPageSpecs', () => {
+  it('should render stuff', () => {
+    expect(1).to.equal(1);
+  });
+});
