3a2353df6c2c942e96bcc1bc9f6ac114d37b576c
Liu Xianning <xianlinbox@gmail.com>
2016-09-21 22:16:52 +0800

    extract styles out of scenes

--- src/property/scenes/propertyView.js
+++ src/property/scenes/propertyView.js
@@ -1,13 +1,12 @@
 'use strict';
 
 import React, {Component} from 'react';
-const {
-  StyleSheet,
+import {
   Image,
   View,
   Text
-} = require('react-native');
-
+} from 'react-native';
+import styles from './styles/propertyViewStyles'
 
 class PropertyView extends Component {
 
@@ -39,38 +38,5 @@ class PropertyView extends Component {
   }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 65
-  },
-  heading: {
-    backgroundColor: '#F8F8F8'
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#DDDDDD'
-  },
-  image: {
-    width: 400,
-    height: 300
-  },
-  price: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    margin: 5,
-    color: '#48BBEC'
-  },
-  title: {
-    fontSize: 20,
-    margin: 5,
-    color: '#656565'
-  },
-  description: {
-    fontSize: 18,
-    margin: 5,
-    color: '#656565'
-  }
-});
 
 module.exports = PropertyView;
---
+++ src/property/scenes/styles/propertyViewStyles.js
@@ -0,0 +1,34 @@
+import {StyleSheet} from 'react-native';
+
+module.exports = StyleSheet.create({
+  container: {
+    marginTop: 65
+  },
+  heading: {
+    backgroundColor: '#F8F8F8'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DDDDDD'
+  },
+  image: {
+    width: 400,
+    height: 300
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 5,
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    margin: 5,
+    color: '#656565'
+  },
+  description: {
+    fontSize: 18,
+    margin: 5,
+    color: '#656565'
+  }
+});
