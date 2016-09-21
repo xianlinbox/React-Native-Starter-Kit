e5fe268de6a1d3c6a516a7be99dbe6e22322f699
Liu Xianning <xianlinbox@gmail.com>
2016-09-21 18:11:37 +0800

    ignore RN upstream warning

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 //@todo remove when RN upstream is fixed
-console.ignoredYellowBox = ['Warning: Failed propType'];
+console.ignoredYellowBox = ['Warning: You are manually calling a React.PropTypes validation'];
 
 import { AppRegistry } from 'react-native';
 import PropertyFinder from './src/app/index';
