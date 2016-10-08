3f350989ebda9874ac1ac6c78bfc27cc4aa695c0
Liu Xianning <xianlinbox@gmail.com>
2016-10-08 13:56:55 +0800

    use action in searchPageDemo

--- storybook/stories/property/components/searchPageDemo.js
+++ storybook/stories/property/components/searchPageDemo.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {storiesOf} from '@kadira/react-native-storybook';
+import {storiesOf, action} from '@kadira/react-native-storybook';
 
 import SearchPage from '../../../../src/property/components/searchPage';
 
 storiesOf('Property', module)
   .add('SearchPage', () => (
-    <SearchPage request={{place_name:"London"}} isLoading={false} search={() => {console.log('Seach called')}}/>
+    <SearchPage request={{place_name:"London"}} isLoading={false} search={action('Search called')}/>
   ));
