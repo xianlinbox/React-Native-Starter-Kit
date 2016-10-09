93ff9922132f890a006d70a78d9a0b232d5e8517
Liu Xianning <xianlinbox@gmail.com>
2016-10-09 15:05:58 +0800

    fix unit test

--- src/property/reducers/__tests__/selectPropertyReducerSpecs.js
+++ src/property/reducers/__tests__/selectPropertyReducerSpecs.js
@@ -4,15 +4,14 @@ import selectPropertyReducer from '../selectPropertyReducer';
 
 test('should return origin state for unhandleable action', t => {
   const notHandableAction = {type: 'NotHandled'};
-  t.deepEqual(selectPropertyReducer(undefined, notHandableAction), {});
+  t.deepEqual(selectPropertyReducer(undefined, notHandableAction), -1);
 });
 
 test('should handle selectProperty action', t => {
-  const expect = {selectedProperty: 1};
   proxyquire.noCallThru();
   const {selectProperty} = proxyquire('../../actions/propertyActions', {
     'react-native-router-flux': {Actions: {}}
   });
 
-  t.deepEqual(selectPropertyReducer(undefined, selectProperty(1)), expect);
+  t.deepEqual(selectPropertyReducer(undefined, selectProperty(1)), 1);
 });
